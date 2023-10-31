b9af64f762661937687213e4fab491817b9ffb0a
HollaNotes <noellewidick@gmail.com>
2023-10-31 09:55:53 -0700

    updating

--- SurfsUp/app_NoelleWatson.py
+++ SurfsUp/app_NoelleWatson.py
@@ -21,6 +21,8 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
+Base.classes.keys()
+
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
@@ -51,31 +53,50 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    
+    # Create session link from Python to DB
+    session = Session(engine)
+
+    # Take year_from_recent from climate.ipynb
     year_from_recent = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
-    precip_date_year_from_recent = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= year_from_recent).all()
+    precip_date_year_from_recent = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_from_recent).all()
+
+    session.close()
 
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
 def stations():
+    # Create session link from Python to DB
+    session = Session(engine)
+
     stations = session.query(Station.station).all()
+
+    session.close()
+
     stations = list(np.ravel(stations))
     return jsonify(stations)
 
 @app.route("/api/v1.0/tobs") 
 def tobs():
+    # Create session link from Python to DB
+    session = Session(engine)
+
     year_from_recent = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     most_active_station_tobs = session.query(Measurement.date, Measurement.tobs).\
     filter(Measurement.station == 'USC00519281').\
     filter(Measurement.date >= year_from_recent).all()
+
+    session.close()
+
     tobs = list(np.ravel(most_active_station_tobs))
     return jsonify(tobs)
 
 
-@app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/<start>/<end>")
-def
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
